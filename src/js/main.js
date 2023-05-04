ec9a3de0119dcca05893edcc8823a93eb33679de
Flavio Alfonzetti <flavioalfonzetti@gmail.com>
2023-05-04 21:19:41 +0200

    removed useless notes

--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,6 @@ const display = document.getElementById('display');
 const key = document.querySelectorAll('.col');
 const delBtn = document.querySelector('.btn-del');
 const resetBtn = document.querySelector('.btn-reset');
-/* const equalBtn = document.querySelector('.btn-equal'); */
 
 // THEMES
 toggle1.addEventListener('click', () => {
@@ -51,6 +50,7 @@ key.forEach((e) => {
           break;
         case '.':
           const lastValue = display.innerHTML.split(/[\+\-\*\/]/).pop();
+          console.log(lastValue);
           if (!lastValue.includes('.') && lastValue.length < maxLength) {
             display.innerHTML += digit;
           }
@@ -81,163 +81,3 @@ delBtn.addEventListener('click', () => {
 resetBtn.addEventListener('click', () => {
   display.innerHTML = '0';
 });
-
-/* import '../scss/styles.scss';
-import * as bootstrap from 'bootstrap';
-
-const body = document.querySelector('body');
-const toggle1 = document.getElementById('toggle-1');
-const toggle2 = document.getElementById('toggle-2');
-const toggle3 = document.getElementById('toggle-3');
-const display = document.getElementById('display');
-const key = document.querySelectorAll('.col');
-const delBtn = document.querySelector('.btn-del');
-const resetBtn = document.querySelector('.btn-reset');
-const equalBtn = document.querySelector('.btn-equal');
-
-//THEMES
-toggle1.addEventListener('click', () => {
-  body.classList.remove('light-theme');
-  body.classList.remove('neon-theme');
-});
-toggle2.addEventListener('click', () => {
-  body.classList.remove('neon-theme');
-  body.classList.add('light-theme');
-});
-toggle3.addEventListener('click', () => {
-  body.classList.add('neon-theme');
-});
-
-//MATH
-key.forEach((e) => {
-  e.addEventListener('click', () => {
-    const digit = e.querySelector('span').textContent;
-    const maxLength = window.innerWidth >= 992 ? 15 : 10;
-
-    if (display.innerHTML.length < maxLength) {
-      switch (digit) {
-        case '=':
-          display.innerHTML = eval(display.innerHTML);
-          break;
-        case '+':
-        case '-':
-        case 'x':
-        case '/':
-          const lastChar = display.innerHTML.slice(-1);
-          if (
-            lastChar !== '+' &&
-            lastChar !== '-' &&
-            lastChar !== 'x' &&
-            lastChar !== '/'
-          ) {
-            display.innerHTML += digit;
-          }
-          break;
-        default:
-          if (!isNaN(digit)) {
-            if (display.innerHTML === '0') {
-              display.innerHTML = digit;
-            } else {
-              display.innerHTML += digit;
-            }
-          }
-      }
-    }
-  });
-});
-
-delBtn.addEventListener('click', () => {
-  const displayValue = display.textContent;
-
-  if (displayValue.length > 1) {
-    display.textContent = displayValue.slice(0, -1); // Remove the last character from display
-  } else {
-    display.textContent = '0';
-  }
-});
-
-resetBtn.addEventListener('click', () => {
-  display.innerHTML = '0';
-}); */
-
-/* import '../scss/styles.scss';
-import * as bootstrap from 'bootstrap';
-
-const body = document.querySelector('body');
-const toggle1 = document.getElementById('toggle-1');
-const toggle2 = document.getElementById('toggle-2');
-const toggle3 = document.getElementById('toggle-3');
-const display = document.getElementById('display');
-const key = document.querySelectorAll('.col');
-const delBtn = document.querySelector('.btn-del');
-const resetBtn = document.querySelector('.btn-reset');
-const equalBtn = document.querySelector('.btn-equal');
-const MAX_CHARS = 15;
-
-//THEMES
-toggle1.addEventListener('click', () => {
-  body.classList.remove('light-theme');
-  body.classList.remove('neon-theme');
-});
-toggle2.addEventListener('click', () => {
-  body.classList.remove('neon-theme');
-  body.classList.add('light-theme');
-});
-toggle3.addEventListener('click', () => {
-  body.classList.add('neon-theme');
-});
-
-//MATH
-key.forEach((e) => {
-  e.addEventListener('click', () => {
-    const digit = e.querySelector('span').textContent;
-
-    switch (digit) {
-      case '=':
-        let input = display.innerHTML.replace(/x/g, '*');
-        display.innerHTML = eval(input);
-        break;
-      case '+':
-      case '-':
-      case 'x':
-      case '/':
-        const lastChar = display.innerHTML.slice(-1);
-        if (
-          lastChar !== '+' &&
-          lastChar !== '-' &&
-          lastChar !== 'x' &&
-          lastChar !== '/'
-        ) {
-          display.innerHTML += digit;
-        }
-        break;
-      default:
-        if (!isNaN(digit)) {
-          if (display.innerHTML.length < MAX_CHARS) {
-            if (display.innerHTML === '0') {
-              display.innerHTML = digit;
-            } else {
-              display.innerHTML += digit;
-            }
-          }
-        }
-    }
-  });
-});
-
-
-delBtn.addEventListener('click', () => {
-  const displayValue = display.textContent;
-
-  if (displayValue.length > 1) {
-    display.textContent = displayValue.slice(0, -1); // Remove the last character from display
-  } else {
-    display.textContent = '0';
-  }
-});
-
-resetBtn.addEventListener('click', () => {
-  display.innerHTML = '0';
-});
-
- */
